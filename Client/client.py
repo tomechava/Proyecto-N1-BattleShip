303ed9f6f76c1c553cbc5b4d570687d811604ece
tomechava <techavarrg@eafit.edu.co>
2025-04-08 20:38:56 -0500

    aaaaa

--- Client/client.py
+++ Client/client.py
@@ -90,6 +90,5 @@ def main():
     except Exception as e:
         print(f"❌ Ocurrió un error inesperado: {e}")
 
-def __init__():
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
